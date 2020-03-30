4fb3e82176423b044e4b9bfccd1925f946f0ae03
Cahandar <cahandar_quliyev96@mail.ru>
2020-03-30 21:56:55 +0400

    ilk yukleme

---
+++ script.js
@@ -0,0 +1,66 @@
+let nxt = document.getElementById("next");
+let prv = document.getElementById("prev");
+let Slider={
+   sekiller : ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"]
+};
+let Say = Slider.sekiller.length;
+let i = 1;
+Deyis(Slider.sekiller[i]);
+function Deyis(x){
+    document.getElementById("slider").style.backgroundImage =`url("img/${x}")`;
+}
+
+nxt.onclick=function(){
+    clearInterval(time);
+    Next();
+    time =setInterval(Next,4000);
+    artim=0;
+    zaman();
+}
+
+
+function Next(){
+    if(i<Say-1){
+        i++
+        Deyis(Slider.sekiller[i]);
+        
+        artim=0;
+        zaman();
+    }else{
+        
+        i=1;
+        Deyis(Slider.sekiller[i]);
+    }
+}
+
+prv.onclick=function(){
+
+    if(i==0){
+        
+        i=Say-1;
+        Deyis(Slider.sekiller[i]);
+        
+    } else{
+        artim=0;
+        zaman();
+        i--;
+        Deyis(Slider.sekiller[i]);
+        
+    }
+}
+let time =setInterval(Next ,4000);
+
+let xett=document.getElementById("xett");
+let artim=0;
+
+setInterval(zaman,40);
+
+function zaman(){
+    if(artim <800){
+        artim+=8;
+        xett.style.width=artim + "px";
+    }else{
+        artim=0;
+    }
+    
+}
